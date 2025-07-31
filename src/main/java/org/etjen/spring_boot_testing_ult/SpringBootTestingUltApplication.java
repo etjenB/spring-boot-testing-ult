e180654324d03b4b5c4a251c17208991996d0490
etjen <buljubasicetjen4@gmail.com>
2025-07-31 12:03:33 +0200

    Initial commit

---
+++ src/main/java/org/etjen/spring_boot_testing_ult/SpringBootTestingUltApplication.java
@@ -0,0 +1,13 @@
+package org.etjen.spring_boot_testing_ult;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootTestingUltApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootTestingUltApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/etjen/spring_boot_testing_ult/SpringBootTestingUltApplicationTests.java
@@ -0,0 +1,13 @@
+package org.etjen.spring_boot_testing_ult;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootTestingUltApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
